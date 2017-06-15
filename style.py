0dacef9fe3f332677faa193fedc3372cee3033be
Kenneth Mathis <kenneth.mathis99@gmail.com>
2017-06-15 16:48:58 +0200

    5: Auto stash before merge of "PyQt5" and "origin/PyQt5"

---
+++ style.py
@@ -0,0 +1,18 @@
+
+
+class Colors:
+    def __init__(self):
+        print("style; Colors; init: Colors class init called")
+
+    def setWorkareaBG(self, R, G, B, A):
+        print("style; Colors; setWorkareaBG:  WIP")
+
+class HandleStyleConfig:
+    def __init__(self):
+        print("style; HandleStyleConfig; init: Init called")
+
+    def adjustConfigFile(self, whatToChange):
+        #Add file handler to take care of the config!
+
+    def readConfig(self):
+        #read the config file
---
+++ testing.py
@@ -0,0 +1,4 @@
+import style
+
+BGColorRed = style.Colors.workareaBGRed
+print(BGColorRed)
