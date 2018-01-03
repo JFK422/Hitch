520db9965be00faf613534e7957444c9b28b9fca
JFK422 <kenneth.mathis99@gmail.com>
2018-01-03 17:49:17 +0100

    The displaying of the project images is done, but needs more backend work in order to be called done

--- components/carousel/carousel.py
+++ components/carousel/carousel.py
@@ -5,10 +5,13 @@ import qtawesome as qta
 import sys
 
 class Carousel(QtWidgets.QWidget):
-    
+    itemsLay = None
+
     def setup(self, carouselWidgets, startPos = 0, elemAsButtons = False):
         self.cWidgets = carouselWidgets
         self.pos = startPos
+
+        print(len(carouselWidgets))
 
         frameWidget = QtWidgets.QWidget()
         frameWidget.setObjectName("carouselFrame")
@@ -21,12 +24,14 @@ class Carousel(QtWidgets.QWidget):
         goLeft.setIconSize(QtCore.QSize(64, 64))
         goLeft.setMaximumWidth(70)
         goLeft.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding))
+        goLeft.clicked.connect(lambda:Carousel.prevItem(self))
 
         goRight = QtWidgets.QPushButton(right, "")
         goRight.setObjectName("carouselSideButtons")
         goRight.setIconSize(QtCore.QSize(64, 64))
         goRight.setMaximumWidth(70)
         goRight.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding))
+        goRight.clicked.connect(lambda:Carousel.nextItem(self))
 
         backLay = QtWidgets.QHBoxLayout()
         backLay.setContentsMargins(QtCore.QMargins(0,0,0,0))
@@ -35,17 +40,37 @@ class Carousel(QtWidgets.QWidget):
         cLay = QtWidgets.QGridLayout()
         cLay.setContentsMargins(QtCore.QMargins(0,0,0,0))
         cLay.addWidget(goLeft, 0, 0)
-        if elemAsButtons:
-            itmBtn = QtWidgets.QPushButton(self.cWidgets[self.pos])
-            cLay.addWidget(itmBtn, 0, 1)
-        else:
-            cLay.addWidget(self.cWidgets[self.pos], 0, 1)
+
+        centerItem = QtWidgets.QWidget()
+        Carousel.itemsLay = QtWidgets.QStackedLayout()
+        Carousel.itemsLay.setContentsMargins(QtCore.QMargins(0,0,0,0))
+        centerItem.setLayout(Carousel.itemsLay)
+        cLay.addWidget(centerItem, 0, 1)
+
+        for i in range(len(self.cWidgets)):
+            if elemAsButtons:
+                itmBtn = QtWidgets.QPushButton(self.cWidgets[i])
+                Carousel.itemsLay.addWidget(itmBtn)
+            else:
+                Carousel.itemsLay.addWidget(self.cWidgets[i])
+
         cLay.addWidget(goRight, 0, 2)
         cLay.setSpacing(10)
         frameWidget.setLayout(cLay)
 
-
         self.setLayout(backLay)
+
+    def nextItem(self):
+        self.pos += 1
+        if(self.pos >= len(self.cWidgets)):
+            self.pos = 0
+        Carousel.itemsLay.setCurrentIndex(self.pos)
+
+    def prevItem(self):
+        self.pos -= 1
+        if(self.pos < 0):
+            self.pos = len(self.cWidgets)-1
+        Carousel.itemsLay.setCurrentIndex(self.pos)
 
 #For executing this file standalone
 if __name__ == '__main__':
--- components/carousel/carouselItem.py
+++ components/carousel/carouselItem.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
+from PIL import Image
 import colorama as clr
 import qtawesome as qta
 import sys
@@ -8,8 +9,7 @@ class CarouselItem(QtWidgets.QWidget):
     def setup(self, name = "", path = "", existing = True):
         self.name = name
         self.projPath = path
-        projFile = open(path, "r")
-        projImg = QtGui.QPixmap(projFile.read().split("\n")[3].split("=")[1])
+        projImg = ""
 
         frameWidget = QtWidgets.QWidget()
         if existing:
@@ -21,15 +21,40 @@ class CarouselItem(QtWidgets.QWidget):
         pName.setObjectName("projectLabel")
         pImg = QtWidgets.QLabel("")
         pImg.setObjectName("projectLabel")
-        pImg.setPixmap(projImg)
+        #pImg.setScaledContents(True)
+
+        #projFile = open(path, "r")
+        #print(projFile.read())
+        #print(projFile.read().split("\n")[3].split("=")[1])
+        #QtGui.QPixmap(projFile.read().split("\n")[3].split("=")[1])
+        im = Image.open("/home/jfk422/Projects/HitchTestProj/Resources/titleImg.png")
+        pic = QtGui.QPixmap("/home/jfk422/Projects/HitchTestProj/Resources/titleImg.png")
+        print(im.size)
+        pImg.setPixmap(pic.scaled(256, 256, QtCore.Qt.KeepAspectRatio))
 
         mainLay = QtWidgets.QVBoxLayout()
-        mainLay.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignCenter)
-        mainLay.setContentsMargins(QtCore.QMargins(0,10,0,10))
-        frameWidget.setLayout(mainLay)
+        mainLay.setContentsMargins(QtCore.QMargins(0,0,0,0))
+        mainLay.setSpacing(0)
 
-        mainLay.addWidget(pName)
-        mainLay.addWidget(pImg)
+        titleLay = QtWidgets.QVBoxLayout()
+        titleLay.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignHCenter)
+        titleLay.setContentsMargins(QtCore.QMargins(0,10,0,10))
+        titleWidget = QtWidgets.QWidget()
+        titleWidget.setLayout(titleLay)
+
+        imgLay = QtWidgets.QVBoxLayout()
+        imgLay.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignHCenter)
+        imgLay.setContentsMargins(QtCore.QMargins(0,0,0,20))
+
+        imgWidget = QtWidgets.QWidget()
+        imgWidget.setLayout(imgLay)
+
+        titleLay.addWidget(pName)
+        imgLay.addWidget(pImg)
+
+        mainLay.addWidget(titleWidget)
+        mainLay.addWidget(imgWidget)
+        frameWidget.setLayout(mainLay)
 
         backLay = QtWidgets.QHBoxLayout()
         backLay.setContentsMargins(QtCore.QMargins(0,10,0,10))
